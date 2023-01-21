ae312c1194d26f3b6a360a4ca924ce4c33cd966c
Dobry-kot <dlputilin@dobry-kot.ru>
2023-01-21 10:10:21 +0300

    [main] formating

--- modules/keycloak-vault/locals.tf
+++ modules/keycloak-vault/locals.tf
@@ -1,12 +1,13 @@
 locals {
 
-  policy_names = flatten([
-  for policy_key, policy_value in vault_policy.auth : 
-    {"${policy_value.name}": ""}
-    ]
-  )
+    policy_names = flatten([
+        for policy_key, policy_value in vault_policy.auth : 
+            {"${policy_value.name}": ""}
+        ]
+    )
     policy_names_map = { for item in local.policy_names :
       keys(item)[0] => values(item)[0]
     }
-  policy_name_list = keys(local.policy_names_map)
+
+    policy_name_list = keys(local.policy_names_map)
 }
