6f6e5f19dffd6eed33e85137080b31797b76692c
Dobry-kot <dlputilin@dobry-kot.ru>
2023-01-21 10:08:37 +0300

    [main] fix

--- modules/keycloak-vault/locals.tf
+++ modules/keycloak-vault/locals.tf
@@ -5,7 +5,7 @@ locals {
     {"${policy_value.name}": ""}
     ]
   )
-    policy_names_map = { for item in local.policy_names_map :
+    policy_names_map = { for item in local.policy_names :
       keys(item)[0] => values(item)[0]
     }
   policy_name_list = keys(local.policy_names_map)
