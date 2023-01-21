67f62c3e025eda45dcbf2654b92cb3cc8533ae84
Dobry-kot <dlputilin@dobry-kot.ru>
2023-01-21 11:31:04 +0300

    [main] fix

--- modules/k8s-yandex-base/data.tf
+++ modules/k8s-yandex-base/data.tf
@@ -1,8 +1,8 @@
 data "yandex_resourcemanager_cloud" "current" {
-  name = var.cloud.extra-aargs.name
+  name = var.cloud.extra-args.name
 }
 
 data "yandex_resourcemanager_folder" "current" {
-  name     = var.cloud.extra-aargs.folder_name
+  name     = var.cloud.extra-args.folder_name
   cloud_id = data.yandex_resourcemanager_cloud.current.id
 }
