24ad529b64793df4830f2a74a98737b14b7f832e
Dobry-kot <dlputilin@dobry-kot.ru>
2023-01-21 11:30:18 +0300

    [main] fix

--- modules/vault-root-pki/vault.core.pki.tf
+++ modules/vault-root-pki/vault.core.pki.tf
@@ -1,21 +1,21 @@
 resource "vault_mount" "core_root_ca" {
-  path                      = var.root-pki.key.path
+  path                      = var.root-pki.extra-args.path
   type                      = "pki"
-  description               = var.root-pki.key.description
-  default_lease_ttl_seconds = var.root-pki.key.default_lease_ttl_seconds
-  max_lease_ttl_seconds     = var.root-pki.key.max_lease_ttl_seconds
+  description               = var.root-pki.extra-args.description
+  default_lease_ttl_seconds = var.root-pki.extra-args.default_lease_ttl_seconds
+  max_lease_ttl_seconds     = var.root-pki.extra-args.max_lease_ttl_seconds
 }
 
 resource "vault_pki_secret_backend_root_cert" "core_root" {
   backend              = vault_mount.core_root_ca.path
-  type                 = var.root-pki.key.type
-  common_name          = var.root-pki.key.common_name
-  ttl                  = var.root-pki.key.ttl
-  format               = var.root-pki.key.format
-  private_key_format   = var.root-pki.key.private_key_format
-  key_type             = var.root-pki.key.key_type
-  key_bits             = var.root-pki.key.key_bits
-  exclude_cn_from_sans = var.root-pki.key.exclude_cn_from_sans
-  province             = var.root-pki.key.province
+  type                 = var.root-pki.extra-args.type
+  common_name          = var.root-pki.extra-args.common_name
+  ttl                  = var.root-pki.extra-args.ttl
+  format               = var.root-pki.extra-args.format
+  private_key_format   = var.root-pki.extra-args.private_key_format
+  key_type             = var.root-pki.extra-args.key_type
+  key_bits             = var.root-pki.extra-args.key_bits
+  exclude_cn_from_sans = var.root-pki.extra-args.exclude_cn_from_sans
+  province             = var.root-pki.extra-args.province
 }
 
