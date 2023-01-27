a1c17c9dde2bbe92dc2a80d101638e5e0bb8d286
Dobry-kot <dlputilin@dobry-kot.ru>
2023-01-27 05:04:19 +0300

    [main] fix lockbox

--- modules/k8s-yandex-master-infra/yandex.lock.box.all.tf
+++ modules/k8s-yandex-master-infra/yandex.lock.box.all.tf
@@ -12,7 +12,7 @@ resource "yandex_lockbox_secret_version" "master_key_keeper_approles_role_id_all
     dynamic "entries" {
         for_each = local.map_role_id_all
         content {
-            key        = entries.key
+            key        = replace(entries.key, "-", "-${local.extra_cluster_name}-")
             text_value = module.k8s-vault-master.role_id_all[entries.key].role_id
         }
     }
@@ -31,7 +31,7 @@ resource "yandex_lockbox_secret_version" "master_key_keeper_approles_secret_id_a
     dynamic "entries" {
         for_each = local.map_secret_id_alls
         content {
-            key        = entries.key
+            key        = replace(entries.key, "-", "-${local.extra_cluster_name}-")
             text_value = module.k8s-vault-master.secret_id_all[entries.key].secret_id
         }
     }
