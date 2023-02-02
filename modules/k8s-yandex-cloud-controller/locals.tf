941bdcc4bb7b5e08b6bfd3cc6eaff9325aa9180e
Dobry-kot <dlputilin@dobry-kot.ru>
2023-02-02 14:57:51 +0300

    [main] fix

--- modules/k8s-yandex-cloud-controller/locals.tf
+++ modules/k8s-yandex-cloud-controller/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  k8s_api_server_port = local.global_vars.kubernetes-ports.kube-apiserver-port-lb
-  pod_cidr            = local.global_vars.k8s_network.pod_cidr
-  cluster_name        = local.global_vars.cluster_metadata.cluster_name
+  k8s_api_server_port = var.global_vars.kubernetes-ports.kube-apiserver-port-lb
+  pod_cidr            = var.global_vars.k8s_network.pod_cidr
+  cluster_name        = var.global_vars.cluster_metadata.cluster_name
 }
