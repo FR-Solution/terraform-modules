4f75893653756f3e108cfe11471c276439ea6b87
Dobry-kot <dlputilin@dobry-kot.ru>
2023-04-09 03:06:29 +0300

    [main]

--- modules/k8s-yandex-cluster-infra/main.tf
+++ modules/k8s-yandex-cluster-infra/main.tf
@@ -31,18 +31,18 @@ module "k8s-masters" {
   cloud_metadata = var.cloud_metadata
 }
 
-module "k8s-masters-firewall" {
-  count = try(var.global_vars.firewall.enabled, false) == true ? 1 : 0
+# module "k8s-masters-firewall" {
+#   count = try(var.global_vars.firewall.enabled, false) == true ? 1 : 0
 
-  depends_on = [
-    module.k8s-masters
-  ]
-  source                      = "../k8s-master-infra-firewall"
-  cluster_instances_internal  = module.k8s-masters.cluster_internal_instances_map
-  cluster_instances_external  = module.k8s-masters.cluster_external_instances_map
-  cluster_api_ip              = try(module.k8s-masters.kube-apiserver-lb, "")
-  k8s_global_vars             = module.k8s-global-vars
-}
+#   depends_on = [
+#     module.k8s-masters
+#   ]
+#   source                      = "../k8s-master-infra-firewall"
+#   cluster_instances_internal  = module.k8s-masters.cluster_internal_instances_map
+#   cluster_instances_external  = module.k8s-masters.cluster_external_instances_map
+#   cluster_api_ip              = try(module.k8s-masters.kube-apiserver-lb, "")
+#   k8s_global_vars             = module.k8s-global-vars
+# }
 
 module "k8s-ready-status" {
   depends_on = [
