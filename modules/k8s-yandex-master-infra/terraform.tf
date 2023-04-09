162205a9ccb84c1cfda750371bc211257d38b8aa
Dobry-kot <dlputilin@dobry-kot.ru>
2023-04-09 03:05:30 +0300

    [main]

--- modules/k8s-yandex-master-infra/terraform.tf
+++ modules/k8s-yandex-master-infra/terraform.tf
@@ -5,10 +5,10 @@ terraform {
       source = "yandex-cloud/yandex"
       version = "0.81.0"
     }
-    sgroups = {
-       version = "1.0.3"
-       source = "fraima/charlotte"
-    }
+    # sgroups = {
+    #    version = "1.0.3"
+    #    source = "fraima/charlotte"
+    # }
   }
   required_version = ">= 0.13"
 
