54801aa130dbd4fd750cca2d8af194169acf8a72
Dobry-kot <dlputilin@dobry-kot.ru>
2023-04-16 19:11:34 +0300

    [main]

--- modules/helm-certmanager/main.tf
+++ modules/helm-certmanager/main.tf
@@ -2,11 +2,11 @@ module "certmanager" {
 
   source = "../helm-temlate"
 
-  release_name  = try(var.extra_values.release_name   ,var.release_name)
-  chart_repo    = try(var.extra_values.chart_repo     ,var.chart_repo)
-  chart_name    = try(var.extra_values.chart_name     ,var.chart_name)
-  chart_version = try(var.extra_values.chart_version  ,var.chart_version)
-  namespace     = try(var.extra_values.namespace      ,var.namespace)
+  release_name  = try(var.extra_values.release.release_name   ,var.release_name)
+  chart_repo    = try(var.extra_values.release.chart_repo     ,var.chart_repo)
+  chart_name    = try(var.extra_values.release.chart_name     ,var.chart_name)
+  chart_version = try(var.extra_values.release.chart_version  ,var.chart_version)
+  namespace     = try(var.extra_values.release.namespace      ,var.namespace)
 
   global_vars   = var.global_vars
   extra_values  = local.merge_values
