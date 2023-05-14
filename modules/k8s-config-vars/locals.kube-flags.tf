6233beeec950af77c69e5a89f78690899cb1b38c
Dobry-kot <dlputilin@dobry-kot.ru>
2023-05-14 20:56:59 +0300

    [TFM-51]

--- modules/k8s-config-vars/locals.kube-flags.tf
+++ modules/k8s-config-vars/locals.kube-flags.tf
@@ -1,8 +1,8 @@
 locals {
   kube_flags = {
-    kube_apiserver_flags          = data.utils_deep_merge_yaml.kube_apiserver_flags.output
-    kube_controller_manager_flags = data.utils_deep_merge_yaml.kube_controller_manager_flags.output
-    kube_scheduler_flags          = data.utils_deep_merge_yaml.kube_scheduler_flags.output
-    kubelet_config_flags          = data.utils_deep_merge_yaml.kubelet_config_flags.output
+    kube_apiserver_flags          = yamlencode(yamldecode(data.utils_deep_merge_yaml.kube_apiserver_flags.output))
+    kube_controller_manager_flags = yamlencode(yamldecode(data.utils_deep_merge_yaml.kube_controller_manager_flags.output))
+    kube_scheduler_flags          = yamlencode(yamldecode(data.utils_deep_merge_yaml.kube_scheduler_flags.output))
+    kubelet_config_flags          = yamlencode(yamldecode(data.utils_deep_merge_yaml.kubelet_config_flags.output))
   }
 }
