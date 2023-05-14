197958de05b487ee908cb57f485fd41e9d8cf210
Dobry-kot <dlputilin@dobry-kot.ru>
2023-05-14 20:54:52 +0300

    [TFM-51]

--- modules/k8s-config-vars/locals.kube-scheduler.tf
+++ modules/k8s-config-vars/locals.kube-scheduler.tf
@@ -7,7 +7,7 @@ locals {
 data "utils_deep_merge_yaml" "kube_scheduler_flags" {
   input = [
     yamlencode(local.default_kube_scheduler_flags),
-    file("${path.module}/default/kube-controller-manager.yaml"),
+    file("${path.module}/default/kube-scheduler.yaml"),
     yamlencode(try(var.extra_args.kube_scheduler_flags, {}))
   ]
 }
