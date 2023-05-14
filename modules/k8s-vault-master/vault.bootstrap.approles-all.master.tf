580ded85b6a9631d075a5ed2e2d9f09c5465c79b
Dobry-kot <dlputilin@dobry-kot.ru>
2023-05-14 19:25:44 +0300

    [TFM-49]

--- modules/k8s-vault-master/vault.bootstrap.approles-all.master.tf
+++ modules/k8s-vault-master/vault.bootstrap.approles-all.master.tf
@@ -11,9 +11,13 @@ resource "vault_approle_auth_backend_role" "all_masters" {
 }
 
 resource "vault_approle_auth_backend_role_secret_id" "all_masters" {
-  count = try(var.k8s_vault_master_secre_id.enabled, false) == true ? 1 : 0
 
-  for_each  = var.k8s_global_vars.master_vars.master_instance_list_map
+  for_each = { 
+    for k, v in var.k8s_global_vars.master_vars.master_instance_list_map: 
+      k => v 
+    if try(var.k8s_vault_master_secre_id.enabled, false) == true
+  }
+
   backend   = var.k8s_global_vars.global_path.base_vault_path_approle
   role_name = vault_approle_auth_backend_role.all_masters[each.key].role_name
 }
