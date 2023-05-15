18e2bee8e75fcbf2e926d867ecfa7b45757514df
fraima <107264732+fraima@users.noreply.github.com>
2023-05-15 21:46:57 +0300

    [TMF-56] (#57)

--- modules/k8s-config-vars/locals.kubelet-config-flags.tf
+++ modules/k8s-config-vars/locals.kubelet-config-flags.tf
@@ -1,10 +1,9 @@
-
 locals {
   default_kubelet_config_flags = {
     staticPodPath       = "${local.global_path.base_static_pod_path}"
     tlsCertFile         = "${local.ssl.intermediate["kubernetes-ca"].issuers["kubelet-server"].certificates["kubelet-server"].key-keeper-args.host_path}/kubelet-server.pem"
     tlsPrivateKeyFile   = "${local.ssl.intermediate["kubernetes-ca"].issuers["kubelet-server"].certificates["kubelet-server"].key-keeper-args.host_path}/kubelet-server-key.pem"
-    healthzPort         = "${local.kubernetes-ports.kubelet-healthz-port }"
+    healthzPort         = local.kubernetes-ports.kubelet-healthz-port
     authentication = {
         x509 = {
             clientCAFile = "${local.ssl.intermediate["kubernetes-ca"].host_path}/kubernetes-ca.pem"
@@ -24,4 +23,3 @@ data "utils_deep_merge_yaml" "kubelet_config_flags" {
     yamlencode(try(var.extra_args.kubelet_config_flags, {}))
   ]
 }
-
